b41d26b4e9d39109d9aebd55db3dfa85172a65d3
Xavier_Marquez <xavier.ray.marquez@gmail.com>
2022-05-16 14:15:08 -0700

    finished starting questions and starting data get and post

--- server.js
+++ server.js
@@ -7,3 +7,62 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const connection = require("./db/database");
+const { getDepartments, getEmployees, getRoles } = require("./db/query.js");
+const { listen } = require("express/lib/application");
+
+function startingQuestion() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "search",
+      message: "what would you like to do?",
+      choices: [
+        "View All Employees",
+        "Add Employee",
+        "Update Employee Role",
+        "View All Roles",
+        "Add Role",
+        "View All Departments",
+        "Add Department",
+        "Exit",
+      ],
+    })
+    .then(function (answer) {
+      switch (answer.search) {
+        case "View all departments":
+          viewAllDepartments();
+          break;
+
+        case "View all roles":
+          viewAllRoles();
+          break;
+
+        case "View all employees":
+          viewAllEmployees();
+          break;
+
+        case "Add a department":
+          addDepartment();
+          break;
+
+        case "Add a role":
+          addRole();
+          break;
+
+        case "Add an employee":
+          addEmployee();
+          break;
+
+        case "Update an Employee Role":
+          updateEmployeeRole();
+          break;
+
+        case "Exit":
+          console.log("See Ya!");
+          connection.end();
+          break;
+      }
+    });
+}
