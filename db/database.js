b49c5c1fcd6829eeb308af8fe2e89b402d567c62
Xavier_Marquez <xavier.ray.marquez@gmail.com>
2022-05-16 13:29:13 -0700

    fixing password connection adding .env file and created querys

--- db/database.js
+++ db/database.js
@@ -1,17 +1,21 @@
 // connect to data base
 const mysql = require("mysql2");
+require("dotenv").config();
+const Sequelize = require("sequelize");
 
-const connection = mysql.createConnection({
-  host: "localhose",
-
-  user: "root",
-  password: "!Seattle12",
-  database: "",
-});
-
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("Successfully Connected to Database!");
-});
+const connection = new Sequelize(
+  // Database name
+  process.env.DB_NAME,
+  // User
+  process.env.DB_USER,
+  // Password
+  process.env.DB_PASSWORD,
+  {
+    // Database location
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  }
+);
 
 module.exports = connection;
